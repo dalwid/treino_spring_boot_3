b519b47afe93b0718853349b6d3d2abb3f05f7f2
David Teixeira Pereira Ferreira <davidbenavraham.ti@gmail.com>
2025-01-21 23:03:37 -0300

    prperties, Game, ORM

---
+++ src/main/java/com/dslist/dslist/entities/Game.java
@@ -0,0 +1,112 @@
+package com.dslist.dslist.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_game")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long    id;
+    private String  title;
+
+    @Column(name = "game_year")
+    private Integer year;
+    private String  genere;
+    private String  platform;
+    private String  imgUrl;
+    private String  shorDescription;
+    private String  longDescription;
+
+    public Game(){}
+
+    public Game(Long id, String title, Integer year, String genere, String platform, String imgUrl, String shorDescription, String longDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.genere = genere;
+        this.platform = platform;
+        this.imgUrl = imgUrl;
+        this.shorDescription = shorDescription;
+        this.longDescription = longDescription;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getGenere() {
+        return genere;
+    }
+
+    public void setGenere(String genere) {
+        this.genere = genere;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShorDescription() {
+        return shorDescription;
+    }
+
+    public void setShorDescription(String shorDescription) {
+        this.shorDescription = shorDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Game game = (Game) o;
+        return Objects.equals(id, game.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
